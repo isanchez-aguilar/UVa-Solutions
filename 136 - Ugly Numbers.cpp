01da0c360d83b22cf816e9c5a492f89e14a6259f
isanchez-aguilar <saais31@gmail.com>
2017-07-07 14:51:03 -0500

    136 - Ugly Numbers
    UVA Online Judge

---
+++ 136 - Ugly Numbers.cpp
@@ -0,0 +1,17 @@
+/*
+*	User: Isanchez_Aguilar
+*	Problem: UVA 136 - Ugly Numbers
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	cout << "The 1500'th ugly number is 859963392.\n";
+
+	return 0;
+}
