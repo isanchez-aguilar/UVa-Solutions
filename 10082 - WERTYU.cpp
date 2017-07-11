ad6d743312be3b7207bfc11f271449ba54e09e97
isanchez-aguilar <saais31@gmail.com>
2017-07-11 02:07:39 -0500

    10082 - WERTYU
    UVA Online Judge

---
+++ 10082 - WERTYU.cpp
@@ -0,0 +1,63 @@
+/*
+*	User: Isanchez_Aguilar
+*	Problem: UVA 10082 - WERTYU
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	
+	string s;
+	const char leftLetters[] = {'A', 'V', 'X', 'S', 'W', 'D', 'F', 'G', 'U', 'H', 'J', 'K', 'N', 'B', 'I', 'O', 'Q', 'E', 'A', 'R', 'Y', 'C', 'Q', 'Z', 'T', 'Z'};
+
+	while(getline(cin, s))
+	{
+		int length = s.length();
+		for(int i = 0; i < length; ++i)
+		{
+			if(isalpha(s[i]))
+				cout << leftLetters[s[i] - 'A'];
+			else if(isdigit(s[i]))
+			{
+				if(s[i] == '0')
+					cout << "9";
+				else if(s[i] == '1')
+					cout << "`";
+				else
+					cout << (char)(s[i] - 1);
+			}
+			else
+			{
+				if(s[i] == ' ')
+					cout << " ";
+				else if(s[i] == '-')
+					cout << "0";
+				else if(s[i] == '=')
+					cout << "-";
+				else if(s[i] == '[')
+					cout << "P";
+				else if(s[i] == ']')
+					cout << "[";
+				else if(s[i] == '\\')
+					cout << "]";
+				else if(s[i] == ';')
+					cout << "L";
+				else if(s[i] == '\'')
+					cout << ";";
+				else if(s[i] == ',')
+					cout << "M";
+				else if(s[i] == '.')
+					cout << ",";
+				else
+					cout << ".";
+			}
+		}
+		cout << "\n";
+	}
+
+	return 0;
+}
