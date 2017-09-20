09f58c8da0187526941d5b56c4acbcaf84920aee
isanchez-aguilar <saais31@gmail.com>
2017-09-19 20:00:06 -0500

    12210 - A Match Making Problem
    UVA Online Judge

---
+++ 12210 - A Match Making Problem.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n;
+	int m;
+	int t = 0;
+	while(cin >> n >> m and (n or m))
+	{
+		vector<int> bachelor(n);
+		vector<int> spinster(m);
+		int minBachelor = INT_MAX;
+
+		for(int i = 0; i < n; ++i)
+		{
+			cin >> bachelor[i];
+			minBachelor = min(minBachelor, bachelor[i]);
+		}
+
+		for(int i = 0; i < m; ++i)
+			cin >> spinster[i];
+
+		cout << "Case " << ++t << ": ";
+		if(n <= m)
+			cout << "0\n";
+		else
+			cout << n - m << " " << minBachelor << "\n";
+	}
+
+	return 0;
+}
