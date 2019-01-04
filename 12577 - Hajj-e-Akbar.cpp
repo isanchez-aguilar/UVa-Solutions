d5bce608be6de26fb77d5667534dbab4d38fdb0c
Isaac Sanchez Aguilar <saais31@gmail.com>
2019-01-03 22:05:04 -0600

    Update 12577 - Hajj-e-Akbar.cpp
    UVa Online Judge

--- 12577 - Hajj-e-Akbar.cpp
+++ 12577 - Hajj-e-Akbar.cpp
@@ -10,22 +10,19 @@ int main(void)
 {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
+	// Variable to know the number of test.
+	int caseNumber = 1;
 	// Variable to save the name.
 	string name;
-	// Variable to know the number of test.
-	int i = 1;
-	// Read the name.
-	cin >> name;
-	while(name[0] != '*')
+
+	while(cin >> name and name[0] != '*')
 	{
 		// If the name is Hajj.
 		if(name[0] == 'H')
-			cout << "Case " << i++ << ": Hajj-e-Akbar\n";
+			cout << "Case " << caseNumber++ << ": Hajj-e-Akbar\n";
 		// If the name is Umrah.
 		else
-			cout << "Case " << i++ << ": Hajj-e-Asghar\n";
-		// Read the name.
-		cin >> name;
+			cout << "Case " << caseNumber++ << ": Hajj-e-Asghar\n";
 	}
 	return 0;
 }
