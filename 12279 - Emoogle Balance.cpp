3d7186dd41c3bf0e18ee35b1893314f947f63850
Isaac Sanchez Aguilar <saais31@gmail.com>
2019-01-03 21:53:09 -0600

    Update 12279 - Emoogle Balance.cpp
    UVa Online Judge

--- 12279 - Emoogle Balance.cpp
+++ 12279 - Emoogle Balance.cpp
@@ -10,35 +10,31 @@ int main(void)
 {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
+	// Variable to know the number of case.
+	int caseNumber = 1;
 	// Variable to save the number of events.
 	int numberEvents;
-	// Variable to know the number of case.
-	int i = 1;
-	// Read the nummber of events.
-	cin >> numberEvents;
 	// Read while there are events.
-	while(numberEvents)
+	while(cin >> numberEvents and numberEvents != 0)
 	{
 		// Variable to save the emoogle balance.
 		int emoogleBalance = 0;
 		// Variable to save the description of event.
 		int descriptionEvent;
 		// Read the description of events.
-		for(int j = 0; j < numberEvents; ++j)
+		for(int i = 0; i < numberEvents; ++i)
 		{
 			// Read the description of event.
 			cin >> descriptionEvent;
 			// If supposed to give a treat.
-			if(descriptionEvent)
+			if(descriptionEvent != 0)
 				++emoogleBalance;
 			// If has given a treat.
 			else
 				--emoogleBalance;
 		}
 		// Print the emoogle balance.
-		cout << "Case " << i++ << ": " << emoogleBalance << "\n";
-		// Read the nummber of events.
-		cin >> numberEvents;
+		cout << "Case " << caseNumber++ << ": " << emoogleBalance << "\n";
 	}
 	return 0;
-}	
+}
