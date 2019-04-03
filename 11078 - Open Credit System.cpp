b0e549bfce9e657cd77076191a9e4150cee9fd6c
Isaac Sanchez Aguilar <saais31@gmail.com>
2019-04-03 17:21:29 -0600

    11078 - Open Credit System
    UVa Online Judge solution.

---
+++ 11078 - Open Credit System.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	int testCases;
+	cin >> testCases;
+
+	while (testCases--)
+	{
+		int students;
+		cin >> students;
+	
+		vector<int> score(students);
+
+		for (int i = 0; i < students; ++i)
+			cin >> score[i];
+
+		int maxDifference = INT_MIN;
+		int minScoreOfStudent = score[students - 1];
+		
+		for (int i = students - 2; i >= 0; --i)
+		{
+			maxDifference = max(maxDifference, score[i] - minScoreOfStudent);
+			minScoreOfStudent = min(minScoreOfStudent, score[i]);
+		}
+
+		cout << maxDifference << "\n";
+	}
+
+	return 0;
+}
