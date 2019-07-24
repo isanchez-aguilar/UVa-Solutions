1b5991ded135bdb8ade847d1b93c0c6848feb46f
Isaac Sanchez Aguilar <saais31@gmail.com>
2019-07-23 21:05:08 -0500

    259 - Software Allocation

--- 259 - Software Allocation.cpp
+++ 259 - Software Allocation.cpp
@@ -4,130 +4,152 @@
 */
 #include <bits/stdc++.h>
 
-#define MAX 1e9
+#define INF INT_MAX
 
 using namespace std;
 
-typedef vector<int> vi;
-typedef pair<int, int> ii;
-typedef vector<ii> vii;
+class Graph {
+public:
+	int vertexs;
+	vector< vector<int> > adj;
+	vector< vector<int> > capacity;
 
-int f;
-vi parent;
-int adj[38][38];
+	Graph(){}
 
-void augment(int v, int minEdge)
-{
-	if (v == 0)
+	Graph(const int& n):vertexs(n), adj(n), capacity(n, vector<int>(n)){}
+
+	inline void addEdge(int u, int v, int cost)
 	{
-		f = minEdge;
-		return;
-	}
-	else if (parent[v] != -1)
-	{
-		augment(parent[v], min(minEdge, adj[parent[v]][v]));
-		adj[parent[v]][v] -= f;
-		adj[v][parent[v]] += f;
+		adj[u].push_back(v);
+		adj[v].push_back(u);
+		capacity[v][u] = 0;
+		capacity[u][v] = cost;
 	}
 
-	return;
-}
+	inline int getMaxFlow(const int& source, const int& sink)
+	{
+		int flow = 0;
+		int newFlow = 0;
+		vector<int> parents(vertexs);
 
+		while (newFlow = getMinAugmenting(source, sink, parents))
+		{
+			flow += newFlow;
+			int currNode = sink;
+
+			while (currNode != source)
+			{
+				int prevNode = parents[currNode];
+				capacity[prevNode][currNode] -= newFlow;
+				capacity[currNode][prevNode] += newFlow;
+				currNode = prevNode;
+			}
+		}
+
+		return flow;
+	}
+
+	inline int getMinAugmenting(const int& source, const int& sink, vector<int>& parents)
+	{
+		int maxFlow = 0;
+		fill(begin(parents), end(parents), -1);
+
+		parents[source] = source;
+		queue< pair<int, int> > bfs;
+
+		bfs.push(make_pair(source, INF));
+
+		while (not bfs.empty())
+		{
+			pair<int, int> data = bfs.front();
+			int u = data.first, flow = data.second;
+			bfs.pop();
+
+			if (u == sink)
+			{
+				maxFlow = flow;
+				break;
+			}
+
+			for (int v : adj[u])
+			{
+				if (parents[v] == -1 and capacity[u][v] > 0)
+				{
+					parents[v] = u;
+					int newFlow = min(flow, capacity[u][v]);
+					bfs.push(make_pair(v, newFlow));
+				}
+			}
+
+		}
+
+
+		return maxFlow;
+	}
+};
 
 int main(void)
 {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
-	string appInfo;
 
-	while (getline(cin, appInfo))
+	string data;
+
+	while (getline(cin, data))
 	{
+		Graph model(38);
 		int totalApps = 0;
-		memset(adj, 0, sizeof adj);
+		const int sink = 37;
+		const int source = 0;
+
 		do
 		{
-			int nApp = appInfo[1] - '0';
-			int u = appInfo[0] - 'A' + 1;
+			char app = data[0];
+			int appId = int(app - 'A' + 1);
+			int people = int(data[1] - '0');
 
-			
-			adj[0][u] = nApp;
-			totalApps += nApp;
-			
-			for (int i = 3; i < appInfo.length() - 1; ++i)
+			totalApps += people;
+			model.addEdge(source, appId, people);
+
+			for (int i = 3; i < int(data.length()) - 1; ++i)
 			{
-				int v = appInfo[i] - '0' + 27;
-
-				adj[u][v] = MAX;
-				adj[v][37] = 1;
+				int computer = int(data[i] - '0');
+				model.addEdge(appId, computer + 27, 1);
 			}
-
-		} while (getline(cin, appInfo) and appInfo.length());
-
+		} while (getline(cin, data) and data.length() > 0);
 
 
-		int mf = 0;
-		while (true)
+		for (int computer = 0; computer < 10; ++computer)
+			model.addEdge(computer + 27, sink, 1);
+
+		int amountUsed = model.getMaxFlow(source, sink);
+
+		if (totalApps == amountUsed)
 		{
-			f = 0;
-			queue<int> node;
-			vi visited(38, false);
-			
-			node.push(0);
-			visited[0] = true;
-			parent.assign(38, -1);
-
-			while (not node.empty())
+			for (int u = 27; u < 37; ++u)
 			{
-				int u = node.front();
-				node.pop();
+				bool used = false;
 
-				if (u == 37)
-					break;
-
-				for (int v = 0; v < 38; ++v)
+				for (int v = 1; v < 27; ++v)
 				{
-					if (adj[u][v] > 0 and not visited[v])
-					{
-						node.push(v);
-						parent[v] = u;
-						visited[v] = true;
-					}
-				}
-			}
-
-			augment(37, MAX);
-
-			if (not f)
-				break;
-
-			mf += f;
-		}
-
-		if (totalApps == mf)
-		{
-			for (int i = 27; i < 37; ++i)
-			{
-				bool used = false;	
-				for (int j = 1; j < 27; ++j)
-				{
-					if (adj[i][j] == 1)
+					if (model.capacity[u][v] == 1)
 					{
 						used = true;
-						cout << (char)(j + 'A' - 1);
+						cout << char(v - 1 + 'A');
 						break;
 					}
 				}
 
-				if (used)
-					continue;
-
-				cout << "_";
+				if (not used)
+					cout << "_";
 			}
+
 			cout << "\n";
 		}
 		else
-			cout << "!\n"; 
+			cout << "!\n";  
 	}
+
 
 	return 0;
 }
